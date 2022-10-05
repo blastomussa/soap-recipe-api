1b62cce24d6fba11fe8a8885a8b72b92c4cc2bc0
blastomussa <jccourtn@gmail.com>
2022-10-05 13:46:44 -0400

    NewUser model validation

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from re import compile, fullmatch, search
+from pydantic import BaseModel, validator, root_validator
+
 
 # token models
 class Token(BaseModel):
@@ -23,7 +25,51 @@ class User(BaseModel):
 class UserInDB(User):
     hashed_password: str # additional pw fields for database user model
 
+# new user model with validators for username, full_name, email, and password
+class NewUser(BaseModel):
+    username: str
+    full_name: str
+    email: str | None = None
+    password1: str
+    password2: str
     
+    @root_validator
+    def check_passwords(cls, values):
+        pw1, pw2 = values.get('password1'), values.get('password2')
+        # upercase, lowercase, number, special character, between 6-20 chars long
+        regex = compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$")
+        if not search(regex,pw1):
+            raise ValueError('password complexit not met')
+        elif pw1 is not None and pw2 is not None and pw1 != pw2:
+            raise ValueError('passwords do not match')
+        return values
+
+    @validator('username')
+    def check_username(cls, v):
+        if ' ' in v:
+            raise ValueError('username must not contain a space')
+        elif not v.isalnum():
+            raise ValueError('username must be alpha-numeric')
+        return v.title()
+    
+    @validator('full_name')
+    def name_must_contain_space(cls, v):
+        if ' ' not in v:
+            raise ValueError('must contain a space')
+        return v.title()
+
+    @validator('email')
+    def check_email(cls, v):
+        # validated based on RFC5322 specification
+        regex = compile(r"([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])")
+        if not fullmatch(regex, v):
+            raise ValueError('Email must be valid')
+        return v.title()   
+
+
+class Admin(BaseModel):
+    admin: bool 
+
 # items model
 class Items(BaseModel):
     items: list[dict]
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,5 @@
-from models import User, Items
+from pydantic import ValidationError
+from models import User, Items, Admin, NewUser
 from fastapi import APIRouter, Depends, HTTPException, status, Form
 
 from internal.dependencies import get_current_admin_user
@@ -34,35 +35,29 @@ async def read_users(current_user: User = Depends(get_current_admin_user)):
     
 
 
-# NOT WORKING
-# HOW DO I PULL DATA FROM POST AND FORM CORRECTLY ACCORDING TO PYDANTIC MODELS?
-@router.post("",status_code=201, response_model=User)
-async def create_user(username: str, password: str):
-    return {username: password}
+# WORK ON THIS
+@router.post("",status_code=201)
+async def create_user(user: NewUser):
+    try:
+        return {user.username: user.password1}
+    except ValidationError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Unable to validate new user fields.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
 
 @router.patch("/{user_id}", status_code=201, response_model=User)
-async def toggle_user_admin(user_id: int, admin: dict[str,bool], current_user: User = Depends(get_current_admin_user)):
+async def toggle_user_admin(user_id: int, admin: Admin, current_user: User = Depends(get_current_admin_user)):
     client = MongoClient(CONNECTION_STRING)
     validateMongo(client)
-
     query = {'_id': user_id}
-    try:
-        if admin['admin'] == True:
-            update = {'$set':{'admin': True}}
-        elif admin['admin'] == False:
-            update = {'$set':{'admin': False}}
-
-        client.api.Users.update_one(query,update)
-        return client.api.Users.find_one(query)
-    except KeyError:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Bad request",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    
+    update = {'$set':{'admin': True}} if admin.admin else {'$set':{'admin': False}} #ternary operator
+    client.api.Users.update_one(query,update)
+    return client.api.Users.find_one(query)
+   
     
 
     
